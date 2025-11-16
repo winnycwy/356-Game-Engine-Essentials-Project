14595b120e992f36ba2ee5db90840d43d052125a
Khan <timur123ax@gmail.com>
2025-11-16 23:58:18 +0800

    More WIzard stuff

--- 356 - Skyward Ascent/Assets/Scripts/WizardScripts/BossController.cs
+++ 356 - Skyward Ascent/Assets/Scripts/WizardScripts/BossController.cs
@@ -9,6 +9,10 @@ public class BossController : MonoBehaviour
     public BossPhase1_Attacks phase1Controller;
     public BossPhase2_Attacks phase2Controller;
 
+    [Header("Trigger Settings")]
+    public Collider activationTrigger; // Assign a trigger collider in the scene
+    private bool bossActivated = false;
+
     private bool phase2Started = false;
 
     void Start()
@@ -17,6 +21,10 @@ public class BossController : MonoBehaviour
             bossHealth = GetComponent<Health>();
 
         bossHealth.OnHealthChanged += OnBossHealthChanged;
+
+        
+        if (phase1Controller != null)
+            phase1Controller.enabled = false;
     }
 
     private void OnBossHealthChanged(float difference)
@@ -30,7 +38,7 @@ public class BossController : MonoBehaviour
             StartPhase2();
         }
 
-        // BOSS DEATH
+        
         if (current <= 0)
         {
             Debug.Log("BOSS: DEAD — Start Phase 3 later.");
@@ -43,8 +51,25 @@ public class BossController : MonoBehaviour
 
         Debug.Log("BOSS: Switching to PHASE 2");
 
-        // Start Phase 2
+        
         if (phase2Controller != null)
             phase2Controller.StartPhase2();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (bossActivated) return;
+
+        if (other.CompareTag("Player"))
+        {
+            bossActivated = true;
+            Debug.Log("Boss activated!");
+
+            
+            if (phase1Controller != null)
+                phase1Controller.enabled = true;
+
+            
+        }
     }
 }
